b284c5bd4536766987ef5a1aafe3714ab5ab057e
Igor-Gordeev-24-7 <igorgo01@mail.ru>
2023-06-25 00:34:07 +0300

    first

---
+++ script.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("burger").addEventListener("click", function () {
+    document.querySelector("header").classList.toggle("open")
+  })
+})
+
+// Аккордион
+document.addEventListener("DOMContentLoaded", () => {
+  const accordions = document.querySelectorAll(".accordion");
+
+  accordions.forEach((el) => {
+    el.addEventListener("click", (e) => {
+      const self = e.currentTarget;
+      const control = self.querySelector(".accordion__control");
+      const content = self.querySelector(".accordion__content");
+
+      self.classList.toggle("open");
+
+      // если открыт аккордеон
+      if (self.classList.contains("open")) {
+        control.setAttribute("aria-expanded", true);
+        content.setAttribute("aria-hidden", false);
+        content.style.maxHeight = content.scrollHeight + "px";
+      } else {
+        control.setAttribute("aria-expanded", false);
+        content.setAttribute("aria-hidden", true);
+        content.style.maxHeight = null;
+      }
+    });
+  });
+});
